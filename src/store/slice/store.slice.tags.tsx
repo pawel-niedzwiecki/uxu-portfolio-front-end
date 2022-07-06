3f4edfbab7168275b4e5cbde8bf7b63bb7591414
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-06 18:09:52 +0200

    i added slice for tags

--- src/store/slice/store.slice.tags.tsx
+++ src/store/slice/store.slice.tags.tsx
@@ -1,1 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TagsGetType, tagsGetState } from "utils/db/tags/get";
+
+interface TagsState {
+  status: string;
+  tags?: TagsGetType;
+}
+
+const initialState: TagsState = {
+  status: "loading",
+  tags: tagsGetState,
+};
+
+export const tagsSlice = createSlice({
+  name: "tag",
+  initialState,
+  reducers: {
+    getTags: (state: TagsState, action: PayloadAction<TagsState>) => {
+      state = action.payload;
+    },
+  },
+});
+
+export const { getTags } = tagsSlice.actions;
+
+export default tagsSlice.reducer;
